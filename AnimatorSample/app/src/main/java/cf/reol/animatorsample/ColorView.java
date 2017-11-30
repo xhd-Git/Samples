13b8a6d8f352643cb2d1f17a142f98c367e0d22e
reol <baili_xx@163.com>
2017-11-30 19:45:09 +0800

    init commit 2

---
+++ AnimatorSample/app/src/main/java/cf/reol/animatorsample/ColorView.java
@@ -0,0 +1,63 @@
+package cf.reol.animatorsample;
+
+import android.animation.ArgbEvaluator;
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Build;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.animation.LinearInterpolator;
+
+/**
+ * Created by reol on 2017/11/30.
+ */
+
+public class ColorView extends View {
+    private int mColor = Color.RED;
+    public ValueAnimator valueAnimator;
+
+    public ColorView(Context context) {
+        super(context);
+        init();
+    }
+
+    private void init() {
+        valueAnimator = ValueAnimator.ofObject(new ArgbEvaluator(), mColor, Color.rgb(0,0,255));
+        valueAnimator.setDuration(3000);
+        valueAnimator.setInterpolator(new LinearInterpolator());
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                mColor = (int) animation.getAnimatedValue();
+                invalidate();
+            }
+        });
+    }
+
+    public ColorView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public ColorView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public ColorView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init();
+    }
+
+    @Override
+    protected void onDraw(final Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawColor(mColor);
+    }
+}
